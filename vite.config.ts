d3022808ab33f6a179038b743635dff9d4bb590e
seven <ryhme@foxmail.com>
2026-05-09 17:03:10 +0800

    chore: set up extension project

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        background: resolve(__dirname, "src/background/index.ts"),
+        content: resolve(__dirname, "src/content/index.ts"),
+        options: resolve(__dirname, "src/options/index.html")
+      },
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name][extname]"
+      }
+    }
+  },
+  publicDir: "public"
+});
